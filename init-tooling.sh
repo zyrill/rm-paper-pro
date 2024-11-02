5d594e14c140b433ef12fe9817e570dd030d5bee
pkleinejaeger <zyrill@users.noreply.github.com>
2024-11-02 01:25:40 +0100

    [feat] maximize screen brightness

--- init-tooling.sh
+++ init-tooling.sh
@@ -1,1 +1,1 @@
-#!/bin/shmount -o rw,remount /SCRIPT_DIRECTORY="$(dirname $(realpath "$0"))"source $SCRIPT_DIRECTORY/entware-install.sh
+#!/bin/shmount -o rw,remount /SCRIPT_DIRECTORY="$(dirname $(realpath "$0"))"# install entwaresource $SCRIPT_DIRECTORY/entware-install.sh# set screen to maximum brightnesssource $SCRIPT_DIRECTORY/set-max-screen-brightness.sh
